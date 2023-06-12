0babc5014483667d513b400884b4b3b12e4019cb
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-06-12 17:52:06 -0300

    atualizar

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-time');
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-time');
+    navbar.classList.remove('active')
+}

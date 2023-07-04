eb15bd4b553c6a033cbd98fc8832df81f8deefc3
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-07-04 07:22:22 -0300

    atualizar

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,20 @@ window.onscroll = () =>{
     menu.classList.remove('fa-time');
     navbar.classList.remove('active')
 }
+
+  /*Rolagem até o Topo*/
+  const rolagem = ()=> {
+    const html = document.documentElement
+    const seta = document.getElementById('go-top')
+  
+    /* Se a rolagem for maior que 550, a seta aparece 
+    abaixo de 550 esconde */
+    if (html.scrollTop > 350 ) {
+      seta.style.display = 'block'
+    } else {
+      seta.style.display = 'none'
+    }
+  }
+
+  //window.addEventListener('scroll', rolagem)
+window.onscroll = ()=> rolagem()//evento somente para elem q existem no html
